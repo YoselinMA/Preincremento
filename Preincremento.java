b53d423d242e04a6b710b28f2ef79248bbc5898c
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-05-05 22:43:23 -0600

    Create Preincremento.java

---
+++ Preincremento.java
@@ -0,0 +1,13 @@
+public class Preincremento {
+    public static void main(String[] args) {
+        int var = 1;
+        int con = 1;
+        System.out.println(++var);
+        System.out.println(var);
+
+        System.out.println(--con);
+        System.out.println(con);
+
+    }
+    
+}
